687207b15529ba05ccd1592fd055f3e67ac769b4
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-06-22 23:20:14 +0530

    Add files via upload

---
+++ Arrays/clear_list.py
@@ -0,0 +1,10 @@
+# Creating list
+yash = [63, 57, 44, 19]
+print('yash before clear:', yash)
+ 
+# Clearing list
+yash.clear()
+print('yash after clear:', yash)
+
+# Time Complexity: O(1)
+# SPace Complexity: O(1)
