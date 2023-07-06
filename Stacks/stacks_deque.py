16cf3493e22d4be38f181ba22481e526369a1070
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-07-06 21:09:03 +0530

    Add files via upload

---
+++ Stacks/stacks_deque.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+stack = deque()  # create an empty stack
+
+stack.append(10)# push elements in stack
+stack.append(20)
+stack.append(30)
+
+print(stack.pop())  
+print(stack.pop())   
+print(stack.pop())   
