8f6501f8c11b55592bbd672db6b57bf248b24733
Yash Bhagwani <83640238+YashB63@users.noreply.github.com>
2023-07-06 21:08:45 +0530

    Add files via upload

---
+++ Stacks/stacks_basic.py
@@ -0,0 +1,8 @@
+x = ["Kolar", "Gold", "Fields"]  # Stack using List 
+x.append("Rocky")   # pushing elements in stacks
+x.append("Bhai")   
+print(x)   
+print(x.pop())   
+print(x)   
+print(x.pop())   
+print(x)   
